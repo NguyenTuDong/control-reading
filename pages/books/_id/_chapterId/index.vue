88d9977479f63e795d276b4e0ce294d579180cd6
NguyenTuDong <tudong.ng@gmail.com>
2020-11-04 13:40:42 +0700

    Update gesture reading feature

--- pages/books/_id/_chapterId/index.vue
+++ pages/books/_id/_chapterId/index.vue
@@ -38,16 +38,28 @@
       </div>
     </div>
     <div class="c-control">
-      <button class="c-control__button" @click="togglePanel"><i class="fas fa-cog"></i></button>
+      <button class="c-control__button" @click="togglePanel">
+        <i class="fas fa-cog"></i>
+      </button>
       <div class="c-control__panel">
-        <label class="c-switch" for="gesture_switch">Reading gesture: 
-          <input v-model="readingGesture" type="checkbox" id="gesture_switch">
+        <label class="c-switch" for="gesture_switch"
+          >Gesture reading:
+          <input v-model="readingGesture" type="checkbox" id="gesture_switch" />
           <span class="c-switch__slider"></span>
         </label>
-        <label class="c-slider" for="scroll_speed">Scrolling speed: <span class="u-primary-color">{{scrollSpeed}}</span>
-          <input v-model="scrollSpeed" type="range" min="20" max="100" id="scroll_speed">
+        <label class="c-slider" for="scroll_speed"
+          >Scroll speed: <span class="u-primary-color">{{ scrollSpeed }}</span>
+          <input
+            v-model="scrollSpeed"
+            type="range"
+            min="20"
+            max="100"
+            id="scroll_speed"
+          />
         </label>
-        <p v-if="loadingModel">Loading...</p>
+      </div>
+      <div class="c-control__loading" :class="{ show: loading }">
+        Loading gesture...
       </div>
     </div>
   </main>
@@ -61,7 +73,7 @@ import modelJSON from '~/data/model.json'
 import metadataJSON from '~/data/metadata.json'
 import { mapState } from 'vuex'
 
-const URL = "https://teachablemachine.withgoogle.com/models/2-TLkuF-X/";
+const URL = 'https://teachablemachine.withgoogle.com/models/2-TLkuF-X/'
 
 export default {
   async asyncData({ route }) {
@@ -71,168 +83,200 @@ export default {
       )
       return { chapter: chapter.data }
     } catch (error) {
-      console.log(error);
+      console.log(error)
       return { chapter: {} }
     }
   },
   data() {
     return {
       model: null,
-      webcam: null,
       isActive: true,
       loadedModel: false,
-      loadingModel: false,
+      loading: false,
       animation: false,
       delay: 0,
     }
   },
   computed: {
     readingGesture: {
-      get () {
-        return this.$store.state.readingGesture;
+      get() {
+        return this.$store.state.readingGesture
       },
-      set (value) {
-        let self = this;
+      set(value) {
+        let self = this
         self.$store.commit('toggleReadingGesture', value)
-        let val = self.readingGesture ? '1' : '0';
-        self.setCookie('readingGesture', val);
-        if(value) {
-          self.init();
+        let val = self.readingGesture ? '1' : '0'
+        self.setCookie('readingGesture', val)
+        if (value) {
+          self.init()
         }
-      }
+      },
     },
     scrollSpeed: {
-      get () {
-        return this.$store.state.scrollSpeed;
+      get() {
+        return this.$store.state.scrollSpeed
       },
-      set (value) {
-        let self = this;
+      set(value) {
+        let self = this
         self.$store.commit('setScrollSpeed', value)
-        self.setCookie('scrollSpeed', self.scrollSpeed);
-      }
-    }
+        self.setCookie('scrollSpeed', self.scrollSpeed)
+      },
+    },
+    webcam: {
+      get() {
+        return this.$store.state.webcam
+      },
+      set(value) {
+        let self = this
+        self.$store.commit('setWebcam', value)
+      },
+    },
   },
   mounted() {
-    let self = this;
-    if(!self.getCookie('readingGesture')) {
-      let value = self.readingGesture ? '1' : '0';
-      self.setCookie('readingGesture', value);
+    let self = this
+    if (!self.getCookie('readingGesture')) {
+      let value = self.readingGesture ? '1' : '0'
+      self.setCookie('readingGesture', value)
     } else {
-      let value = self.getCookie('readingGesture') == '1' ? true : false;
+      let value = self.getCookie('readingGesture') == '1' ? true : false
       this.$store.commit('toggleReadingGesture', value)
     }
     if (!self.getCookie('scrollSpeed')) {
-      self.setCookie('scrollSpeed', self.scrollSpeed);
+      self.setCookie('scrollSpeed', self.scrollSpeed)
     } else {
-      let value = parseInt(self.getCookie('scrollSpeed'));
+      let value = parseInt(self.getCookie('scrollSpeed'))
       self.$store.commit('setScrollSpeed', value)
     }
-    if(this.isActive && this.readingGesture) this.init();
+    if (this.isActive && this.readingGesture) this.init()
   },
-  beforeDestroy() {
-    this.isActive = false;
-    if (this.webcam) this.webcam.pause();
+  async beforeDestroy() {
+    this.isActive = false
+    if (this.$route.name != 'books-id-chapterId') {
+      if (this.webcam) {
+        await this.webcam.stop()
+        this.webcam = null
+      }
+    }
   },
   methods: {
     togglePanel() {
-      $('.c-control__panel').slideToggle(300);
+      $('.c-control__panel').slideToggle(300)
     },
-    setCookie(name,value,days) {
-        var expires = "";
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days*24*60*60*1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    setCookie(name, value, days) {
+      var expires = ''
+      if (days) {
+        var date = new Date()
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+        expires = '; expires=' + date.toUTCString()
+      }
+      document.cookie = name + '=' + (value || '') + expires + '; path=/'
     },
     getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
+      var nameEQ = name + '='
+      var ca = document.cookie.split(';')
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+      }
+      return null
     },
-    eraseCookie(name) {   
-        document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    eraseCookie(name) {
+      document.cookie =
+        name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
     },
     async init() {
-      let self = this;
-      if(!this.loadedModel) {
-        this.loadingModel = true;
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
+      let self = this
+      setTimeout(() => {
+        this.loading = true
+      }, 0)
+      if (!this.loadedModel) {
+        const modelURL = URL + 'model.json'
+        const metadataURL = URL + 'metadata.json'
         // load the model and metadata
         // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
         // Note: the pose library adds a tmPose object to your window (window.tmPose)
-        self.model = await tmPose.load(modelURL, metadataURL)
-
+        let model = await tmPose.load(modelURL, metadataURL)
+        this.model = model
+        this.loadedModel = true
+      }
+      if (!this.webcam) {
         // Convenience function to setup a webcam
         const size = 200
         const flip = true // whether to flip the webcam
-        self.webcam = new tmPose.Webcam(size, size, flip) // width, height, flip
-        await self.webcam.setup() // request access to the webcam
-        await self.webcam.play()
-        this.loadedModel = true;
-        this.loadingModel = false;
+        this.webcam = new tmPose.Webcam(size, size, flip) // width, height, flip
+        await this.webcam.setup() // request access to the webcam
+        await this.webcam.play()
       }
+      setTimeout(() => {
+        this.loading = false
+      }, 0)
       window.requestAnimationFrame(self.loop)
     },
     async loop(timestamp) {
-      let { webcam, predict, isActive, readingGesture, loop } = this;
+      let { webcam, predict, isActive, readingGesture, loop } = this
       if (!this.animation) {
         webcam.update() // update the webcam frame
         await predict()
       } else {
-        this.delay++;
+        this.delay++
         if (this.delay == 100) {
-          this.animation = false;
-          this.delay = 0;
+          this.animation = false
+          this.delay = 0
         }
       }
-      if(isActive && readingGesture) {
+      if (isActive && readingGesture) {
         window.requestAnimationFrame(loop)
       }
     },
     async predict() {
-      let self = this;
-      let className = '';
+      let self = this
+      let className = ''
       // Prediction #1: run input through posenet
       // estimatePose can take in an image, video or canvas html element
-      const { pose, posenetOutput } = await self.model.estimatePose(self.webcam.canvas)
+      const { pose, posenetOutput } = await self.model.estimatePose(
+        self.webcam.canvas
+      )
       // Prediction 2: run input through teachable machine classification model
       const prediction = await self.model.predictTopK(posenetOutput, 1)
-      className = prediction[0].className;
+      className = prediction[0].className
 
-      console.log(className);
+      console.log(className)
       if (className == 'NotLook') {
-        self.animation = true;
-          $('body').addClass('hide');
+        self.animation = true
+        $('body').addClass('hide')
       } else {
         if ($('body').hasClass('hide')) {
-          self.animation = true;
-          $('body').removeClass('hide');
+          self.animation = true
+          $('body').removeClass('hide')
         }
       }
 
-      let scrollTop = $(window).scrollTop();
-      let scrollSpeed = parseInt(self.scrollSpeed);
+      let scrollTop = $(window).scrollTop()
+      let scrollSpeed = parseInt(self.scrollSpeed)
 
       if (className == 'LookUp' && scrollTop > scrollSpeed) {
-        self.animation = true;
-        $('html, body').stop().animate({
-          scrollTop: scrollTop - scrollSpeed
-        }, 800)
+        self.animation = true
+        $('html, body')
+          .stop()
+          .animate(
+            {
+              scrollTop: scrollTop - scrollSpeed,
+            },
+            800
+          )
       }
 
       if (className == 'LookDown') {
-        self.animation = true;
-        $('html, body').stop().animate({
-          scrollTop: scrollTop + scrollSpeed
-        }, 800)
+        self.animation = true
+        $('html, body')
+          .stop()
+          .animate(
+            {
+              scrollTop: scrollTop + scrollSpeed,
+            },
+            800
+          )
       }
     },
   },
@@ -321,9 +365,11 @@ export default {
     font-size: 20px;
     outline: none;
     cursor: pointer;
-    -moz-box-shadow: 0 0 15px rgba(0,0,0,0.1);
-    -webkit-box-shadow: 0 0 15px rgba(0,0,0,0.1);
-    box-shadow: 0 0 15px rgba(0,0,0,0.1);
+    -moz-box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+    -webkit-box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+    position: relative;
+    z-index: 10;
     @media only screen and (max-width: 450px) {
       width: 40px;
       height: 40px;
@@ -337,9 +383,9 @@ export default {
     width: 300px;
     background-color: white;
     padding: 20px;
-    -moz-box-shadow: 0 0 15px rgba(0,0,0,0.1);
-    -webkit-box-shadow: 0 0 15px rgba(0,0,0,0.1);
-    box-shadow: 0 0 15px rgba(0,0,0,0.1);
+    -moz-box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+    -webkit-box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
     border-radius: 10px;
     display: none;
     label {
@@ -351,6 +397,31 @@ export default {
     @media only screen and (max-width: 450px) {
       bottom: 50px;
       width: 230px;
+      padding: 10px;
+    }
+  }
+  &__loading {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 240px;
+    background-color: white;
+    padding: 15px 20px;
+    -moz-box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+    -webkit-box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    opacity: 0;
+    pointer-events: none;
+    transition: 500ms ease-in-out;
+    &.show {
+      right: 60px;
+      opacity: 1;
+      @media only screen and (max-width: 450px) {
+        right: 50px;
+      }
+    }
+    @media only screen and (max-width: 450px) {
       padding: 10px;
     }
   }
@@ -400,39 +471,39 @@ export default {
   }
 }
 .c-slider {
-  input[type=range] {
+  input[type='range'] {
     -webkit-appearance: none;
     width: 100%;
-    border-radius: 15px;  
+    border-radius: 15px;
     outline: none;
     margin-top: 15px;
     // Chrome
-		@media screen and (-webkit-min-device-pixel-ratio:0) {
-			& {
-				overflow: hidden;
-				height: 12px;
-				-webkit-appearance: none;
-				background-color: #ddd;
-			}
-			&::-webkit-slider-runnable-track {
-				height: 12px;
-				-webkit-appearance: none;
-				color: #444;
-				// margin-top: -1px;
-				transition: box-shadow 0.2s ease-in-out;
-			}
-			&::-webkit-slider-thumb {
-				width: 12px;
-				-webkit-appearance: none;
-				height: 12px;
-				cursor: ew-resize;
-				background: #ee434c;
-				box-shadow: -340px 0 0 330px #ee434c, inset 0 0 0 2px #fff;
-				border-radius: 50%;
-				transition: box-shadow 0.2s ease-in-out;
-				position: relative;
-			}
-		}
+    @media screen and (-webkit-min-device-pixel-ratio: 0) {
+      & {
+        overflow: hidden;
+        height: 12px;
+        -webkit-appearance: none;
+        background-color: #ddd;
+      }
+      &::-webkit-slider-runnable-track {
+        height: 12px;
+        -webkit-appearance: none;
+        color: #444;
+        // margin-top: -1px;
+        transition: box-shadow 0.2s ease-in-out;
+      }
+      &::-webkit-slider-thumb {
+        width: 12px;
+        -webkit-appearance: none;
+        height: 12px;
+        cursor: ew-resize;
+        background: #ee434c;
+        box-shadow: -340px 0 0 330px #ee434c, inset 0 0 0 2px #fff;
+        border-radius: 50%;
+        transition: box-shadow 0.2s ease-in-out;
+        position: relative;
+      }
+    }
   }
   @media only screen and (max-width: 450px) {
     font-size: 14px;
